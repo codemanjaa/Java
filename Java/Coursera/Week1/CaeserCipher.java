5fd5b350928f44b968a6fd28f154876b6948fb58
Codemanja <codemanjaa@gmail.com>
2017-06-20 16:42:48 +0300

    Update CaeserCipher.java
    Eyeball decryption method adapted!!!

--- Java/Coursera/Week1/CaeserCipher.java
+++ Java/Coursera/Week1/CaeserCipher.java
@@ -1,9 +1,10 @@
+
 public class CaserCipher {
 
 	
 	
 	public static String encrypt(String input, int key){
-		
+		input = input.toUpperCase();
 		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String encryptalpha = alphabet.substring(key) + alphabet.substring(0, key);
 		StringBuffer result= new StringBuffer();
@@ -16,7 +17,7 @@ public class CaserCipher {
 		
 			if(isThere(ch)){
 				int loc =alphabet.indexOf(input.charAt(i));
-				System.out.println(loc);
+				//System.out.println(loc);
 				result.append(encryptalpha.charAt(loc));
 				}
 			
@@ -31,6 +32,8 @@ public class CaserCipher {
 	
 	public static  boolean isThere(char ch){
 		
+		
+		
 		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		
 			int loc = alphabet.indexOf(ch);
@@ -44,13 +47,27 @@ public class CaserCipher {
 		
 		
 	}
+	
+	public static void eyeballDecrypt(String encrypted){
+		
+		//CaserCipher cs = new CaserCipher();
+		
+		for(int k=0; k<26; k++){
+		String result = encrypt(encrypted, k);
+		System.out.println(k + "\t"+result);
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		//String encryptalpha = alphabet.substring(23) + alphabet.substring(0, 23);
 		System.out.println(encrypt("FIRST LEGION ATTACK EAST FLANK!", 23));
-	
+		eyeballDecrypt("LUJYFWAPVU huk zljbypaf hyl mbukhtluahs whyaz vm avkhf'z Pualyula");
+		
+		
 		//System.out.println(isThere('!'));
 		
 	}
